00b0e22a11c8a1d9fcdcb227f93c5660e75971e6
JoanaVNB <joanavidon@gmail.com>
2023-05-12 08:37:46 -0300

    modifiquei struct Exchange

--- presenter/presenter.go
+++ presenter/presenter.go
@@ -1,13 +1,13 @@
 package presenter
 
-import "exchange/domain"
+import "exchange/service"
 
 type Presenter struct{
 	ValorConvertido	float64 `json:"valorConvertido"`
 	SimboloMoeda	string	`json:"simboloMoeda"`
 }
 
-func PresenterFX(e domain.Exchange) *Presenter{
+func PresenterFX(e service.Exchange) *Presenter{
 	return &Presenter{
 		ValorConvertido:	e.ValueConverted,
 		SimboloMoeda:		e.SymbolTo,
